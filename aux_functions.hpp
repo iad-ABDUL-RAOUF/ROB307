386a5f9aeb08fc0857fea140fece398d1a712058
MAlphaBecker <madeleine.becker@enta-paristech.fr>
2020-11-29 12:35:22 +0100

    Creating header

---
+++ aux_functions.hpp
@@ -0,0 +1,4 @@
+#include <assert.h> //source code : https://code.woboq.org/userspace/glibc/assert/assert.h.html
+#include <stdlib.h> //pour malloc. contenu dans le compilateur 
+
+float*** sum_flow(int* size,float*** DS_pointHomoIm2, float*** DS_pointHomoIm1, float*** DS_VHomoIm1);
